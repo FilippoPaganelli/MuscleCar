4d824713ba2d436efac87abeca4dd81862358cd0
FilippoPaganelli <32205866+FilippoPaganelli@users.noreply.github.com>
2021-10-29 13:11:20 +0200

    update

--- Arduino Code/RC_transmitter/RC_transmitter.ino
+++ Arduino Code/RC_transmitter/RC_transmitter.ino
@@ -10,21 +10,28 @@
 // Serial.println(myIMU.readFloatGyroY(), 4);
 // Serial.print(" Z = ");
 // Serial.println(myIMU.readFloatGyroZ(), 4);
+// (?) suppose it goes from 0 to 180 (clockwise) and 0 to -180 (counterclockwise)
+// we could read it and use to normalise the speed value for the motors
 
 #include <printf.h>
 #include <RF24_config.h>
 #include <SPI.h>
 #include <nRF24L01.h>
 #include <RF24.h>
+////#include "SparkFunLSM6DS3.h"
 
 RF24 radio(7, 8); // CE, CSN
+////LSM6DS3 myIMU;
 
 const byte address[6] = "00001";
 int button_pin = 3;
 boolean button_state = 0;
-const char text[] = " :D ";
-int readData = 0;
-int threshold = 600;
+//const char text[] = " :D ";
+//int readData = 0;
+//int threshold = 600;
+
+////float rotationValue = 0;
+int speedValue = 0;
 
 void setup() {
   Serial.begin(9600);
@@ -33,12 +40,17 @@ void setup() {
   radio.openWritingPipe(address);
   radio.setPALevel(RF24_PA_MIN);
   radio.stopListening();
+  ////myIMU.begin();
 }
 
 void loop() {
-  readData = analogRead(A0);
-  Serial.println(readData);
-  if(readData > threshold)
+  //readData = analogRead(A0);
+
+  speedValue = analogRead(A0);
+  ////rotation = myIMU.readFloatGyroX() / 180.0;
+  
+  //Serial.println(readData);
+  /*if(readData > threshold)
   {
     button_state = HIGH;
     Serial.println("Analog is HIGH");
@@ -47,18 +59,12 @@ void loop() {
   {
     button_state = LOW;
     Serial.println("Analog is LOW");
-  }
+  }*/
   
-  //button_state = digitalRead(button_pin);
-  radio.write(&button_state, sizeof(button_state));
-  delay(50);
-
-  if (button_state == HIGH) {
-    //radio.write(&text, sizeof(text));
-    //Serial.println("sending..." + fuck);
-    delay(5);
-  }
-  else {
-    delay(5);
-  }
+  //radio.write(&button_state, sizeof(button_state));
+  
+  radio.write(&speedValue, sizeof(speedValue));
+  ////radio,write(&rotationValue, sizeof(rotationValue));
+  
+  delay(55);
 }
